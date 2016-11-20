4d9407c723801b74fd8c2ef0a1798b2ae45b56d6
spacepirate7 <harshaxd@gmail.com>
2016-11-20 16:36:45 +0530

    Update 9.whohasthemostchange.m

--- 1-to-10/9.whohasthemostchange.m
+++ 1-to-10/9.whohasthemostchange.m
@@ -1,8 +1,5 @@
 function b = most_change(a)
-w=[0.25 0.05 0.1 0.01];
-  r=repmat(w,size(a,1),1);
-  p=r.*a
-  s=sum(p,2)
-[M,I] = max(s(:))
-b=I
+  w=[0.25; 0.05; 0.1; 0.01]
+  [M,I] = max(a*w)
+    b=I
 end
